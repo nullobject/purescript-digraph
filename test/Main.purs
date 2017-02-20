9f9d88b3fe473c9b50183317d34c9522659e7360
Josh Bassett <josh.bassett@gmail.com>
2017-02-20 21:45:36 +1100

    Update deps

--- test/Main.purs
+++ test/Main.purs
@@ -3,12 +3,10 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Node.Process (PROCESS())
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (RunnerEffects, run)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Data.GraphSpec (graphSpec)
 
-main :: forall e. Eff (console :: CONSOLE, process :: PROCESS | e) Unit
+main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] graphSpec
